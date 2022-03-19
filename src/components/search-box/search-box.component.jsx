586c6b166dd248428e1757d1b60a250b25278c5f
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-03-19 17:11:11 -0500

    added Card component and styling; added header to App with styling

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -7,6 +7,7 @@ class SearchBox extends Component {
     return (
       <input
         className={this.props.className}
+        type="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
       />
